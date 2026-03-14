2bf7a7b4581fc28da9a95fb5b0dd8f2a7627008c
Hamid Mammadov <memmedovhemid72@gmail.com>
2026-03-14 17:10:33 +0400

    fix: connect database PostgreSQL best professional bd

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
