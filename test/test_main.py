1778b6e670da0c2754a8bd0918909a8089d375cc
Hamid Mammadov <104730137+hamidmammadov1711@users.noreply.github.com>
2026-03-18 23:32:56 +0400

    fix: setup directory for testing

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,13 @@
-from fastapi.testclient import TestClient
-import main
+"""Test cases for the FastAPI application defined in main.py."""
 from fastapi import status
+from fastapi.testclient import TestClient
 
-client = TestClient(main.app)
+from main import app
+
+client = TestClient(app)
+
 
 def test_healthy():
     response = client.get("/helathy")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "API is healthy and running!"}
-
-
