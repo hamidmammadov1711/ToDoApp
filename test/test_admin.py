00055193b506dd30f0ba82ea884eb8811245e997
Hamid Mammadov <104730137+hamidmammadov1711@users.noreply.github.com>
2026-03-22 10:41:49 +0400

    fix: FastAPI Project Test Part 6 Wrap Up Admin tests

--- test/test_admin.py
+++ test/test_admin.py
@@ -3,9 +3,11 @@ from fastapi import status
 
 from dependencies import get_db, get_current_user
 from main import app
+from models import Todos
 from .conftest import (
     override_get_db,
     override_get_current_user,
+    TestingSessionLocal,
     client,
     test_todo
 )
@@ -22,3 +24,17 @@ def test_admin_read_all_authenticated(test_todo):
                                 'description': 'Need to learn everyday!',
                                 'id': 1,
                                 'priority': 5, 'owner_id': 1}]
+
+
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == 204
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_admin_delete_not_found():
+    response = client.delete("/admin/todo/9999")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Todo not found'}
