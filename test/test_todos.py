fe15996e2b3f326b7343e7a2ad39e2bcfa494e56
Hamid Mammadov <104730137+hamidmammadov1711@users.noreply.github.com>
2026-03-22 09:56:02 +0400

    fix: FastAPI Project Test Part 4 Create Reusable Utils (conftest.py) File

--- test/test_todos.py
+++ test/test_todos.py
@@ -1,77 +1,20 @@
 """Test cases for the todos API."""
 
-import pytest
 from fastapi import status
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
-from database import Base
 from dependencies import get_db, get_current_user
 from main import app
 from models import Todos
-
-DATABASE_URL = "sqlite:///./testdb.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False},
-                       poolclass=StaticPool,
-
-                       )
-
-TestingSessionLocal = sessionmaker(autocommit=False,
-                                   autoflush=False,
-                                   bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    """Override the database dependency for testing."""
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-def override_get_current_user():
-    """Override the current user dependency for testing."""
-
-    class MockUser:
-        id = 1
-        username = "hamidmammadov"
-        role = "admin"
-
-    return MockUser()
-
+from .conftest import (
+    override_get_db,
+    override_get_current_user,
+    TestingSessionLocal,
+    client,
+    test_todo
+)
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
-
-client = TestClient(app)
-
-
-@pytest.fixture
-def test_todo():
-    db = TestingSessionLocal()
-
-    todo = Todos(
-        title="Learn to code!",
-        description="Need to learn everyday!",
-        priority=5,
-        complete=False,
-        owner_id=1
-    )
-
-    db.add(todo)
-    db.commit()
-
-    yield todo
-
-    with engine.connect() as connection:
-        connection.execute(text("DELETE FROM todos;"))
-        connection.commit()
 
 
 def test_read_all_authenticated(test_todo):
@@ -174,4 +117,4 @@ def test_delete_todo(test_todo):
 def test_delete_todo_not_found():
     response = client.delete("/todos/todo/999")
     assert response.status_code == 404
-    assert response.json()=={"detail": "Todo not found"}
+    assert response.json() == {"detail": "Todo not found"}
