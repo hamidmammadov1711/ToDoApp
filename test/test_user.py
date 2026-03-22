69c32b79eadc763369b4f6103546dfe3214d4423
Hamid Mammadov <104730137+hamidmammadov1711@users.noreply.github.com>
2026-03-22 12:13:56 +0400

    fix: FastAPI Project Unit Test Part 8 Finishing Test User

--- test/test_user.py
+++ test/test_user.py
@@ -2,12 +2,9 @@ from fastapi import status
 
 from dependencies import get_db, get_current_user
 from main import app
-from models import Todos
-from dependencies import verify_password,hash_password
 from .conftest import (
     override_get_db,
     override_get_current_user,
-    TestingSessionLocal,
     client,
     test_user
 )
@@ -19,9 +16,31 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 def test_return_user(test_user):
     response = client.get("/user")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() ['username'] == 'hamidmammadovtest'
-    assert response.json() ['email'] == 'hamidmammadov@email.com'
-    assert response.json() ['first_name'] == 'Hamid'
-    assert response.json() ['last_name'] == 'Mammadov'
-    assert response.json() ['role'] == 'admin'
-    assert response.json() ['phone_number'] == '(994)-531-00-00'
+    assert response.json()['username'] == 'hamidmammadovtest'
+    assert response.json()['email'] == 'hamidmammadov@email.com'
+    assert response.json()['first_name'] == 'Hamid'
+    assert response.json()['last_name'] == 'Mammadov'
+    assert response.json()['role'] == 'admin'
+    assert response.json()['phone_number'] == '(994)-531-00-00'
+
+
+def test_change_password_success(test_user):
+    response = client.put("/user/password", json={
+        "password": "testpassword",
+        "new_password": "newpassword"
+    })
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    response = client.put("/user/password", json={
+        "password": "wrong_test_password",
+        "new_password": "newpassword"
+    })
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {"detail": "Error on password change: Incorrect current password"}
+
+
+def test_change_phone_number_success(test_user):
+    response = client.put("/user/phone_number/2222222222")
+    assert response.status_code == 204
