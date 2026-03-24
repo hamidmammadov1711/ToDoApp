db4b9cd78b50aae53c83e0b2a7521a87ca20df96
Hamid Mammadov <104730137+hamidmammadov1711@users.noreply.github.com>
2026-03-24 17:26:57 +0400

    fix: FastAPI Project Full Stack Application jinja2, setup css, js

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """ FastAPI layihəsi üçün main (əsas) tətbiq faylı. Bu fayl FastAPI tətbiqini işə salır, verilənlər bazasını qurur və identifikasiya və todo idarəetməsi üçün marşrutlaşdırıcıları ehtiva edir. Lazımi modulları idxal edir, müəyyən edilmiş modellərə əsasən verilənlər bazası cədvəllərini yaradır və istifadəçi identifikasiyası və todo əməliyyatları ilə əlaqəli API sorğularını idarə etmək üçün identifikasiya və todo marşrutlaşdırıcılarını ehtiva edir. """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 import models
 from database import engine
@@ -10,10 +12,18 @@ app = FastAPI(
     title="Todo API",
     description="Bu API, istifadəçilərin todo tapşırıqlarını idarə etmələrinə imkan verir. İstifadəçilər qeydiyyatdan keçə və daxil ola bilərlər, sonra isə öz todo tapşırıqlarını yarada, oxuya, yeniləyə və silə bilərlər. Hər bir istifadəçi yalnız öz tapşırıqlarını görə və idarə edə bilər.",
     version="1.0.1",
-    
+
 )
 
 models.Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
 
 @app.get("/healthy")
 def helathy():
@@ -22,6 +32,7 @@ def helathy():
     :return: Bir JSON obyektində "message" açarı ilə API-nin sağlam olduğunu bildirən mesaj.
     """
     return {"message": "API is healthy and running!"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
